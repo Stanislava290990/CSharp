d62885f8f9f42fdabc1b9b34b7913cd6d3f339f2
Anastasiia Pavlova <Stanislava290990@gmail.com>
2023-08-05 15:04:21 +0300

    Task4 added

---
+++ Task04/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все числа от -N до N
+*/
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = -number; i <= number; i++)
+{
+    Console.Write(i + " ");
+}
