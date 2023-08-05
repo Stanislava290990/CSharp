e7ef433265c1c4036da0ec3fd251aed102891628
Anastasiia Pavlova <Stanislava290990@gmail.com>
2023-08-05 15:20:20 +0300

    Task07 added

---
+++ Task07/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 99 && number < 1000)
+{
+    Console.WriteLine(number%10);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
+
