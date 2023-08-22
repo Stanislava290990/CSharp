1157c212de581790a60c6829eb0acbb0410de727
Anastasiia Pavlova <Stanislava290990@gmail.com>
2023-08-22 10:47:33 +0300

    Task18 added

---
+++ Task18/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Напишите программу, которая по заданному номеру четверти,
+ показывает диапазон возможных координат точек в этой 
+ четверти (x и y).
+ */
+System.Console.WriteLine("Введите номер четверти :");
+int quater = Convert.ToInt32(Console.ReadLine());
+
+if (quater ==1)
+{
+   System.Console.WriteLine("x and y +"); 
+}
+else if(quater ==2)
+{
+   System.Console.WriteLine("x - , y +"); 
+}
+else if(quater ==3)
+{
+   System.Console.WriteLine("x and y -"); 
+}
+else if(quater ==2)
+{
+   System.Console.WriteLine("x + , y -"); 
+}
+else
+{
+System.Console.WriteLine("Нет такой четверти ");
+}
