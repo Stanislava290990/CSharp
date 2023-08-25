143968d0fe3a2a08163af74a4c17fc9614ba0a7d
Anastasiia Pavlova <Stanislava290990@gmail.com>
2023-08-25 10:52:48 +0300

    Task28 added

---
+++ Task28/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает на вход число N 
+и выдаёт произведение чисел от 1 до N.
+*/
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+
+int MultOfNumber(int number)
+{
+    int mult = 1;
+for (int i = 1; i <= userNumber; i++)
+{
+    mult *= i;
+}
+    return mult;
+}
+System.Console.WriteLine($"Произведение цифр в числе {userNumber} равна {MultOfNumber(userNumber)}");
