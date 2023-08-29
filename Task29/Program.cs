c0b5203cb18f3f3e09cb53d476c4518279a51163
Anastasiia Pavlova <Stanislava290990@gmail.com>
2023-08-29 10:28:00 +0300

    Task29added

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,19 +3,36 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
-int lenArray = ReadInt("Введите длинну массива: ");
-
-int[] randomArray = new int[lenArray];
-for (int i = 0; i < randomArray.Length; i++)
-{
-    randomArray[i] = new Random().Next(1,9);
-    Console.Write(randomArray[i] + " ");
-}
+Console.WriteLine("Введите размер массива и границу случайных чисел: "); 
+int arrayLength = Convert.ToInt32(Console.ReadLine()); 
+int min = Convert.ToInt32(Console.ReadLine()); 
+int max = Convert.ToInt32(Console.ReadLine()); 
 
 
-// Функция ввода
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
+int [] GetRandomArray (int arrayLength1, int start, int end) 
+{ 
+    int [] array = new int [arrayLength1]; 
+    for (int i=0; i<array.Length; i++) 
+    { 
+        array [i] = new Random().Next(start, end); 
+    } 
+    Возвращаемый массив; 
+} 
+
+Int[] userArray = GetRandomArray(arrayLength, min, max); 
+
+void PrintArray(int [] arrayToPrint) 
+{ 
+    Console.Write("["); 
+    for (int i=0; i<arrayToPrint.Length; i++) 
+    {
+       Console.Write(arrayToPrint[i]); 
+       if (i<arrayToPrint.Length - 1) 
+       { 
+        Console.Write (", "); 
+       } 
+    } 
+    Console.Write("]"); 
+} 
+
+PrintArray (userArray);
