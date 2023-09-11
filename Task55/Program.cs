3113b1ea8959b6f71bc3cb1a0c17717966b95ac1
Anastasiia Pavlova <Stanislava290990@gmail.com>
2023-09-11 09:26:55 +0300

    Task55 added

---
+++ Task55/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Задайте двумерный массив. Напишите программу,
+которая заменяет строки на столбцы. В случае, если это
+невозможно, программа должна вывести сообщение для
+пользователя.
+*/
+Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] arr = FillMatrixRnd(m, n);
+PrintMatrix(arr);
+if (arr.GetLength(0) == arr.GetLength(1))
+{
+    TurnMatrix(arr);
+    PrintMatrix(arr);
+}
+else Console.WriteLine("Невозможно заменить все строки на столбцы.");
+
+int[,] FillMatrixRnd(int row, int col)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void TurnMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i; j < matrix.GetLength(1); j++)
+        {
+            (matrix[i, j], matrix[j, i]) = (matrix[j, i], matrix[i, j]);
+        }
+    }
+}
+
