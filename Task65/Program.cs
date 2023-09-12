d6b5b4b6e6d2be598da36c0467e2d3dce0277da6
Anastasiia Pavlova <Stanislava290990@gmail.com>
2023-09-12 10:09:20 +0300

    task65 added

---
+++ Task65/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+ M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void EnumerationNumbers(int a, int b)
+{
+    if (a == b) Console.Write($"{a} ");
+    else
+    {
+        EnumerationNumbers(a, b - 1);
+        Console.Write($"{b} ");
+    }
+}
+
+if (m > n) EnumerationNumbers(n, m);
+else EnumerationNumbers(m, n);
